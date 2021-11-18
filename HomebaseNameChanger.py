a65ad9efb3235ce82c2408b148edc637918585d5
PRO100KatYT <dominator972@wp.pl>
2021-11-18 13:54:22 +0100

    1.0.1 Update
    Some code improvements.

--- HomebaseNameChanger.py
+++ HomebaseNameChanger.py
@@ -4,10 +4,9 @@ import requests
 # Links that will be used in the later part of code.
 class links:
     getToken = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
-    getHomebaseName = "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{0}/client/QueryProfile?profileId=common_public"
-    setHomebaseName = "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{0}/client/SetHomebaseName?profileId=common_public"
+    profileRequest = "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{0}/client/{1}?profileId={2}"
 
-print("Homebase Name Changer v1.0.0 by PRO100KatYT\n")
+print("Homebase Name Changer v1.0.1 by PRO100KatYT\n")
 
 # Getting the token and using it to login into an account.
 reqToken = requests.post(links.getToken, headers={"Authorization": "basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=", "Content-Type": "application/x-www-form-urlencoded"}, data={"grant_type": "authorization_code", "token_type": "eg1", "code": input("Insert the auth code:\n")})
@@ -24,15 +23,13 @@ else:
 
 # Changing the Homebase name.
 homebaseHeaders = {"Authorization": f"bearer {access_token}", "Content-Type": "application/json"}
-reqGetHomebaseName = requests.post(links.getHomebaseName.format(account_id), headers=homebaseHeaders, data="{}")
-reqSetHomebaseName = requests.post(links.setHomebaseName.format(account_id), headers=homebaseHeaders, json={"homebaseName": input("\nEnter a new Homebase name:\n")})
+reqGetHomebaseName = requests.post(links.profileRequest.format(account_id, "QueryProfile", "common_public"), headers=homebaseHeaders, data="{}")
+reqSetHomebaseName = requests.post(links.profileRequest.format(account_id, "SetHomebaseName", "common_public"), headers=homebaseHeaders, json={"homebaseName": input("\nEnter a new Homebase name:\n")})
 reqGetHomebaseNameText = json.loads(reqGetHomebaseName.text)
 reqSetHomebaseNameText = json.loads(reqSetHomebaseName.text)
 if "errorMessage" in reqSetHomebaseNameText:
-    if "Validation Failed. Invalid fields were [homebaseName]" in reqSetHomebaseNameText['errorMessage']:
-        print("ERROR: The Homebase name can't be blank.")
-    else:
-        print(f"\nERROR: {reqSetHomebaseNameText['errorMessage']}")
+    if "Validation Failed. Invalid fields were [homebaseName]" in reqSetHomebaseNameText['errorMessage']: print("ERROR: The Homebase name can't be blank.")
+    else: print(f"\nERROR: {reqSetHomebaseNameText['errorMessage']}")
     input("\nPress ENTER to close the program.\n")
     exit()
 else:
